43e5638cf6d349ba352cf2411f2f2208998545d4
Jacob <jakeboone@aol.com>
2015-04-20 07:53:09 -0400

    first commit with canvas text

---
+++ main.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+ctx.font = '32px Arial';
+ctx.fillStyle = 'DeepSkyBlue';
+ctx.strokeStyle = 'DarkSlateGray';
+ctx.fillText("Pops Tomatos", 30, 50);
+ctx.strokeText("Pops Tomatos", 30, 50);
+
+
+
+// $(document).on('ready', function() {
+ 
+
+
+// // $('button').onClick(function){
+// // 	draw(0,0)
+// // }
+
+// // var draw = function(x,y){
+// // 	var canvas = document.getElementById('canvas');
+// // 	var ctx = canvas.getContext('2d');
+
+// // }
+
+
+
+
+
+
+// });
